f31468fe09640cc93daa09190eda507573901c97
Yeshwanth Chowdhary Mineni <ymineni@saintpeters.edu>
2026-04-21 16:07:39 -0400

    Fix dim_customers remove invalid app_id column

--- dbt/models/marts/dim_customers.sql
+++ dbt/models/marts/dim_customers.sql
@@ -1,11 +1,14 @@
 select
     customer_id,
-    app_id,
+    email,
+    first_name,
+    last_name,
+    created_at,
+    updated_at,
+    orders_count,
+    state,
+    total_spent,
+    verified_email,
     tags,
-    admin_graphql_api_id,
-    created_at_epoch,
-    created_at_utc,
-    portable_extracted_at,
-    addresses,
-    _airbyte_extracted_at
+    airbyte_extracted_at
 from {{ ref('stg_customers') }}
